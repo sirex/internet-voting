b64b299d3ead63265908ffccc5adba67b82db47a
iakovleva <paulina.iakovleva@gmail.com>
2015-07-14 09:22:36 +0300

    Indentation and other small fixes during code review

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
         README = readme.read()
@@ -7,24 +7,22 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 setup(
     name='internet-voting-system',
     version='0.1',
-    packages=[''],
-	scripts=[''],
-    license='MIT', 
+    packages=find_packages(),
+    license='AGPL',
     description='The prototype of fully end to end verifiable internet voting system.',
     long_description=README,
     url='https://github.com/python-dirbtuves/internet-voting',
     author='Python dirbtuves',
     author_email='python-dirbtuves@googlegroups.com',
     install_requires=[
-		'pycrypto 2.6.1',	
-	#	'cryptography',
+        'cryptography',
     ],
-	entry_points={
-		'console_scripts': ['voter:main'],
-		},
+    entry_points={
+        'console_scripts': ['voter:main'],
+    },
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Programming Language :: Python :: 3'
     ],
 )
--- voter.py
+++ voter.py
@@ -2,18 +2,20 @@ import sys
 import argparse
 
 
-commands = ['register', 'verify voters', 'vote', 'verify my vote', 'verify all votes', 'count votes']
+commands = ['register', 'verify voters', 'vote', 'verify my vote',
+            'verify all votes', 'count votes']
 
 def main(argv):
-	args = sys.argv.pop()
-	command_string = ' '.join(sys.argv[2:])
-	if command_string in commands:		
-		print 'id: ', sys.argv[1], 'command: ', command_string, 'args: ', args
-	else:
-		print "Unknown command. Please enter the valid command."
+
+    args = sys.argv.pop()
+    command_string = ' '.join(sys.argv[2:])
+    if command_string in commands:
+        print 'id: ', sys.argv[1], 'command: ', command_string, 'args: ', args
+    else:
+        print "Unknown command. Please enter the valid command."
 
 if __name__ == "__main__":
-	   main(sys.argv[1:])
+    main(sys.argv[1:])
 
 #parser = argparse.ArgumentParser()
 #parser.add_argument('--arguments')
